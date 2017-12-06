07bd98a04aa01e45ea9a940bc30b074ddeaf7903
Vettias <mathieu.desriac@epitech.eu>
2017-12-06 15:56:02 +0100

    rec

--- src/Game.cpp
+++ src/Game.cpp
@@ -24,23 +24,39 @@ VPoint Game::mapIterator(int deepth) {
   register Point pt = Point();
   register VPoint win = VPoint();
   register VPoint tmp = VPoint();
-  for (pt.x = 0; pt.x < protocol->mapSize().x ; pt.x++) {
-    for (pt.y = 0; pt.y < protocol->mapSize().y ; pt.y++) {
-      if (deepth % 2 == 0)
-        protocol->mapGet(pt) = Tile::OWN;
-      else
-        protocol->mapGet(pt) = Tile::OPPONENT;
+  Tile player;
+  int val;
+
+  win.v = 0;
+  if (deepth % 2 != 0)
+    player = Tile::OWN;
+  else
+    player = Tile::OPPONENT;
+  for (pt.x = 0; pt.x < protocol->mapSize().x ; pt.x += 1) {
+    for (pt.y = 0; pt.y < protocol->mapSize().y ; pt.y += 1) {
+      if (protocol->mapGet(pt) != Tile::EMPTY)
+        continue;
+      protocol->mapGet(pt) = player;
       if (deepth != 0)
       {
-        //evaluate
-        tmp = mapIterator(deepth--);
-        //evaluate - ret
+        tmp = mapIterator(--deepth);
+        //attackEvaluate(pt, player);
       }
-      else
-        (void)pt;//evaluate();
+      // else
+      // {
+        val = attackEvaluate(pt, OWN);
+        if (win.v < val)
+        {
+          win.v = val;
+          win.x = pt.x;
+          win.y = pt.y;
+        }
+      //}
       protocol->mapGet(pt) = Tile::EMPTY;
     }
   }
+  protocol->log("Debug " + std::to_string(win.x) + ", " + std::to_string(win.y) + ", " + std::to_string(win.v));
+  //protocol->log("Debug " + std::to_string(pt.x) + ", " + std::to_string(pt.y));
   return win;
 }
 
@@ -145,28 +161,33 @@ Point Game::randomEmptyPoint()
 
 Point Game::play()
 {
+  VPoint win = VPoint();
   Point pt = Point();
   if (protocol == nullptr) {
     std::cout << "MESSAGE protocol not set in game" << std::endl;
     return pt;
   }
-  Point size = protocol->mapSize();
-  Point curr;
-  char max = 0;
-  for (curr.y = 0; curr.y < size.y; curr.y += 1) {
-    for (curr.x = 0; curr.x < size.x; curr.x += 1) {
-      if (protocol->mapGet(curr) == EMPTY) {
-        int buff = attackEvaluate(curr, OWN);
-        if (buff > max)
-        {
-          pt = curr;
-          max = buff;
-        }
-      }
-    }
-  }
-  if (max == 0) {
+  //Point size = protocol->mapSize();
+  //Point curr;
+  //char max = 0;
+  // for (curr.y = 0; curr.y < size.y; curr.y += 1) {
+  //   for (curr.x = 0; curr.x < size.x; curr.x += 1) {
+  //     if (protocol->mapGet(curr) == EMPTY) {
+  //       int buff = attackEvaluate(curr, OWN);
+  //       if (buff > max)
+  //       {
+  //         pt = curr;
+  //         max = buff;
+  //       }
+  //     }
+  //   }
+  // }
+  win = mapIterator(1);
+  if (win.v == 0) {
     return randomEmptyPoint();
   }
+  protocol->log("Debug " + std::to_string(win.x) + ", " + std::to_string(win.y));
+  pt.x = win.x;
+  pt.y = win.y;
   return pt;
 }
